624bde89b2ba260fe0493553b0ad7289eb092069
devantequick <devantequick94@gmail.com>
2022-03-07 16:05:22 -0500

    dom added

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,8 +4,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+       <h1>Todo App</h1>
+
+      
+       <ul>
+         <li>Cook Breakfast</li>
+         <li>Study all day</li>
+         <li>Workout</li>
+       </ul>
+
+       <form>
+         <input/>
+         <button>Add</button>
+       </form>
+
+       <button>Clear</button>
       </div>
-    )
+    );
   }
 }
