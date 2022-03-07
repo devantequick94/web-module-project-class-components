898252c39226e763e0c9e2fa3892d96f5c9bb609
devantequick <devantequick94@gmail.com>
2022-03-07 16:42:54 -0500

    dom functionality added

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
        <h1>Todo App</h1>
 
       
        <ul>
+         {
+           todos.map(todo=> {
+             return (<li>{todo.task} { todo.completed?<span>- completed</span> : <span></span> }</li>)
+           })
+         }
          <li>Cook Breakfast</li>
          <li>Study all day</li>
          <li>Workout</li>
